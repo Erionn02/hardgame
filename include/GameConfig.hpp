87551db894b2de20baec80ef5a14699985f3cc84
Erionn02 <jkjablonski@gmail.com>
2023-07-24 23:26:36 +0200

    added game config

---
+++ include/GameConfig.hpp
@@ -0,0 +1,6 @@
+struct GameConfig {
+    static inline float horizontal_velocity_tick{200};
+    static inline float vertical_velocity_tick{200};
+    static inline float gravity{600};
+    static inline float jump_velocity{-200};
+};
